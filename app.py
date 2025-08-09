6ba12c941dd68902031c06d999d980c892976555
Llerenas <soporte@terravital.com.mx>
2025-08-09 12:32:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from controllers import controllers as controllers_bp
+from controllers import controllers
 import os  # 👈 necesario para leer el puerto de Railway
 
-    app = Flask(__name__)
-    app.register_blueprint(controllers_bp)
+app = Flask(__name__)
+app.register_blueprint(controllers)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Railway asigna el puerto
